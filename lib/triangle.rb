0d4fde8aec6eda7a251373005c1318e33e3ad1b8
Walter Aab <aabwalter@yahoo.com>
2020-11-19 20:35:29 -0600

    commit tree 88a37b5c4532779b201ef3cccbd7b8faf4979db6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    validate_triangle 
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @c == @a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end  
+
+  def validate_triangle
+    true_triangle = [(b + c > a), (a + b > c), (a + c > b)]
+    [a,b,c].each do |side|
+      true_triangle << false if side <= 0
+    raise TriangleError if true_triangle.include?(false)
+    end
+  end
 end
